251b0da79a727bce54820d8e27f584e2f16cc799
atcamo <atiliocm@gmail.com>
2023-05-20 17:06:50 -0400

    arreglado para netlify

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Footer.module.css';
 
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-        <div className={styles.flourBackground}></div>
-        <div className={styles.content}>
-            <div className={styles.links}>
-            <a href="#">Contacto</a>
-            <a href="#">Sobre Nosotros</a>
-            <a href="#">Ubicación</a>
+            <div className={styles.flourBackground}></div>
+            <div className={styles.content}>
+                <div className={styles.links}>
+                    <Link to="/">Contacto</Link>
+                    <Link to="/">Sobre Nosotros</Link>
+                    <Link to="/">Ubicación</Link>
+                </div>
+                <div className={styles.createdBy}>
+                    &copy; {new Date().getFullYear()} atcamo. Todos los derechos reservados.
+                </div>
             </div>
-            <div className={styles.createdBy}>
-            &copy; {new Date().getFullYear()} atcamo. Todos los derechos reservados.
-            </div>
-        </div>
         </footer>
     );
 };
